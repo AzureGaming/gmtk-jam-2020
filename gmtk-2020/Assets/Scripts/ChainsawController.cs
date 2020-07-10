d2d6261814543261da479347255a3cbc09bb93b8
andrew.lam@rangle.io <andrew.lam@rangle.io>
2020-07-10 18:05:50 -0400

    Add chainsaw, movement and camera follow

---
+++ gmtk-2020/Assets/Scripts/ChainsawController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainsawController : MonoBehaviour
+{
+  public Rigidbody2D rb;
+  public float speed = 1f;
+
+  private void FixedUpdate()
+  {
+    if (rb != null)
+    {
+      ApplyInput();
+    }
+  }
+
+  void ApplyInput()
+  {
+    rb.AddForce(transform.up * speed);
+  }
+}
