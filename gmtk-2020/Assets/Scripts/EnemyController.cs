2282aa727999f7987f49fdfdce5fe8742e34c3ed
andrew.lam@rangle.io <andrew.lam@rangle.io>
2020-07-11 14:31:48 -0400

    Add score and score multiplier

--- gmtk-2020/Assets/Scripts/EnemyController.cs
+++ gmtk-2020/Assets/Scripts/EnemyController.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+  GameManager gameManager;
+
   public AudioSource deathSound;
   public SpriteRenderer spriteRenderer;
   public Rigidbody2D rb;
 
   public int bloodAmount = 10;
+  public int scoreValue = 100;
 
   HealthBar healthBar;
 
   private void Awake()
   {
     healthBar = FindObjectOfType<HealthBar>();
+    gameManager = FindObjectOfType<GameManager>();
   }
 
   public void Die()
   {
+    gameManager.IncrementScore(scoreValue);
     deathSound.Play();
     healthBar.IncrementHealth(bloodAmount);
     spriteRenderer.enabled = false;
---
+++ gmtk-2020/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+  public TMPro.TextMeshProUGUI scoreText;
+  public GameObject scoreUI;
+
+  int score = 0;
+  int multiplier = 1;
+
+  public void ShowScore()
+  {
+    scoreUI.SetActive(true);
+  }
+
+  public void HideScore()
+  {
+    scoreUI.SetActive(false);
+  }
+
+  public void IncrementScore(int scoreValue, int multiplierValue)
+  {
+    SetScore(score + scoreValue, multiplierValue);
+  }
+
+  public void SetScore(int scoreValue, int multiplierValue)
+  {
+    score = scoreValue;
+    multiplier = multiplierValue;
+    SetScoreText(score, multiplierValue);
+  }
+
+  public void SetScore()
+  {
+    SetScoreText(score, multiplier);
+  }
+
+  void SetScoreText(int value, int multiplier)
+  {
+    scoreText.text = "Score: " + value + " x " + multiplier;
+  }
+}
