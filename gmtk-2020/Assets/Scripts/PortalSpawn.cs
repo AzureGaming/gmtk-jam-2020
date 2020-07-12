c115d3034af3a400a74090d28072aa7125801506
andrew.lam@rangle.io <andrew.lam@rangle.io>
2020-07-12 13:43:30 -0400

    More bugfixes

--- gmtk-2020/Assets/Scripts/PortalSpawn.cs
+++ gmtk-2020/Assets/Scripts/PortalSpawn.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PortalSpawn : MonoBehaviour
 {
   public SpriteRenderer spriteRenderer;
+  public BoxCollider2D triggerCollider;
   bool donePortalAnimation = false;
+
+  private void Start()
+  {
+    triggerCollider.enabled = false;
+  }
 
   public IEnumerator PortalDone()
   {
     yield return new WaitUntil(() => donePortalAnimation == true);
+    triggerCollider.enabled = true;
     spriteRenderer.enabled = false;
     Destroy(gameObject, 1.5f);
   }
